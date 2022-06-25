56b8b6b93521c8ee050693d82661522c51b64dbe
fennecj <hwahwa649@gmail.com>
2022-06-25 16:43:51 +0800

    Add ADC example for Joystick module

---
+++ Hardwares/Joystick/obj_socket_24/gnu_arcem9d_wei_r16/src/joystick.o.d
@@ -0,0 +1,3 @@
+obj_socket_24/gnu_arcem9d_wei_r16//./src/joystick.o: src/joystick.c \
+ ../../board/socket/configs/24/core_config.h inc/joystick.h \
+ ../../platform/inc/hx_drv_iic_m.h ../../platform/inc/dev_common.h

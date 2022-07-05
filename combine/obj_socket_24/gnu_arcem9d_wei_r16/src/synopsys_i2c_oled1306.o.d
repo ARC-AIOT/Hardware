7e4ac0401ddf6e4ab25fd47ab1f71eca629e1210
fennecj <hwahwa649@gmail.com>
2022-07-05 11:49:32 +0800

    Remove built file from repo

--- combine/obj_socket_24/gnu_arcem9d_wei_r16/src/synopsys_i2c_oled1306.o.d
+++
@@ -1,4 +0,0 @@
-obj_socket_24/gnu_arcem9d_wei_r16//./src/synopsys_i2c_oled1306.o: \
- src/synopsys_i2c_oled1306.c ../../board/socket/configs/24/core_config.h \
- inc/synopsys_i2c_oled1306.h ../../platform/inc/hx_drv_iic_m.h \
- ../../platform/inc/dev_common.h
--- combine/obj_socket_24/gnu_arcem9d_wei_r16/src/text_detection_tools.o.d
+++
@@ -1,3 +0,0 @@
-obj_socket_24/gnu_arcem9d_wei_r16//./src/text_detection_tools.o: \
- src/text_detection_tools.c ../../board/socket/configs/24/core_config.h \
- inc/text_detection_tools.h inc/tflitemicro_algo.h
